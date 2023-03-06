2bad7ea9509e0ffdadbd7451c74a6986ae9b62f7
TamaraDimov <tamemojsoska@gmail.com>
2023-03-06 22:31:34 +0100

    setup redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
