b6831051e0c4cb8a1cda65a4517919f0a75da9ca
TamaraDimov <tamemojsoska@gmail.com>
2023-03-07 13:10:23 +0100

    add functionalities

---
+++ src/redux/books/list.js
@@ -0,0 +1,18 @@
+const list = [
+  {
+    id: 1,
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+  },
+  {
+    id: 2,
+    title: 'The Lord of the Rings ',
+    author: 'J. R. R. Tolkien',
+  },
+  {
+    id: 3,
+    title: 'Nineteen Eighty Four ',
+    author: 'George Orwell',
+  },
+];
+export default list;
